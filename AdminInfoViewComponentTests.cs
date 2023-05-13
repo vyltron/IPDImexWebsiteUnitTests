c4f8d9d930c64b264640bb18b54e5ba46507b4ca
Dragus Ciprian <cipri.dragus27@gmail.com>
2023-05-13 12:34:52 +0300

    AdminInfo and ControlPanel ViewComponents Tests

---
+++ AdminInfoViewComponentTests.cs
@@ -0,0 +1,38 @@
+﻿using IPDImexWebsite.Models.Repository;
+using IPDImexWebsite.ViewComponents;
+using IPDImexWebsite.ViewModels;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPDImexWebsiteUnitTests
+{
+    [TestFixture]
+    internal class AdminInfoViewComponentTests
+    {
+        [Test]
+        public async Task InvokeAsync_CanCountUnreadAplicationsAndMessages()
+        {
+            //arrange
+            var mockMessages = new Mock<IRepositoryMessage>();
+            mockMessages.Setup(x => x.GetUnreadMessagesCount()).Returns(async () => await Task.FromResult(10));
+
+            var mockApplications = new Mock<IRepositoryAplication>();
+            mockApplications.Setup(x => x.GetUnreadAplicationsCount()).Returns(async () => await Task.FromResult(12));
+
+            var viewComponent = new AdminInfoViewComponent(mockMessages.Object, mockApplications.Object);
+
+            //act
+            var result = (await viewComponent.InvokeAsync() as ViewViewComponentResult)?.ViewData?.Model as AdminInfoViewModel ?? new();
+
+            //assert
+            Assert.That(result.MessagesUnreadCount, Is.EqualTo(10));
+            Assert.That(result.AplicationsUnreadCount, Is.EqualTo(12));
+        }
+    }
+}
---
+++ ControlPanelViewComponentTests.cs
@@ -0,0 +1,37 @@
+﻿using IPDImexWebsite.Models.Repository;
+using IPDImexWebsite.ViewComponents;
+using IPDImexWebsite.ViewModels;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPDImexWebsiteUnitTests
+{
+    [TestFixture]
+    internal class ControlPanelViewComponentTests
+    {
+        [Test]
+        public async Task InvokeAsync_CanCounAllAplicationsAndMessages()
+        {
+            //arrange
+            var mockMessages = new Mock<IRepositoryMessage>();
+            mockMessages.Setup(x => x.GetAllMessagesCount()).Returns(async () => await Task.FromResult(10));
+
+            var mockApplications = new Mock<IRepositoryAplication>();
+            mockApplications.Setup(x => x.GetAllAplicationsCount()).Returns(async () => await Task.FromResult(12));
+
+            var viewComponent = new ControlPanelViewComponent(mockMessages.Object, mockApplications.Object);
+
+            //act
+            var result = (await viewComponent.InvokeAsync() as ViewViewComponentResult)?.ViewData?.Model as ControlPanelViewModel ?? new();
+
+            //assert
+            Assert.That(result.MessagesCount, Is.EqualTo(10));
+            Assert.That(result.AplicationsCount, Is.EqualTo(12));
+        }
+    }
+}
